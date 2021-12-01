d15e427253bb5dd269d70c9adadcd271a39093d0
InviBull <virtualclass.raghav@gmail.com>
2021-12-01 10:43:22 +0530

    first commit

---
+++ ChessMain.py
@@ -0,0 +1,73 @@
+import pygame as p
+import ChessEngine
+
+WIDTH = HEIGHT = 512
+DIMENSION = 8
+SQ_Size = HEIGHT // DIMENSION
+MAX_FPS = 15
+IMAGES = {}
+
+def loadImages():
+    pieces = ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR", "bP","wP", "wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+    for piece in pieces:
+        
+        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_Size, SQ_Size))
+
+
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = ChessEngine.GameState()
+    loadImages()
+    running = True
+    sqSelected = ()
+    playerClicks = []
+
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()
+                col = location[0]//SQ_Size
+                row = location[1]//SQ_Size
+                if sqSelected == (row, col):
+                    sqSelected = ()
+                    playerClicks = []
+                else:
+                    sqSelected = (row,col)
+                    playerClicks.append(sqSelected)
+                if len(playerClicks) == 2:
+                    move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
+                    print(move.getChessNotation())
+                    gs.makeMove(move)
+                    sqSelected = ()
+                    playerClicks = []
+                    
+
+        drawGameState(screen, gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+def drawGameState(screen, gs):
+    drawBoard(screen)
+    drawPieces(screen, gs.board)
+
+def drawBoard(screen):
+    colors = [(235, 235, 208), (119, 148, 85)]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r+c)%2)]
+            p.draw.rect(screen, color, p.Rect(c*SQ_Size, r*SQ_Size, SQ_Size, SQ_Size))
+
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(c*SQ_Size, r*SQ_Size, SQ_Size, SQ_Size))
+
+if __name__ == "__main__":
+    main()
